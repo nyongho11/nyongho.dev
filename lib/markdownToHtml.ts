8127ff06c298ada43552178ba78569d44208c600
Yongho5580 <70843139+Yongho5580@users.noreply.github.com>
2024-03-15 18:50:47 +0900

    Feat: remark, rehype 플러그인 추가

--- lib/markdownToHtml.ts
+++ lib/markdownToHtml.ts
@@ -1,25 +1,35 @@
 // src/lib/markdown.ts
 
-import toc from "@jsdevtools/rehype-toc";
-import rehypeHighlight from "rehype-highlight";
-import rehypeSlug from "rehype-slug";
-import rehypeStringify from "rehype-stringify";
-import remarkRehype from "remark-rehype";
-import remarkParse from "remark-parse";
-import { unified } from "unified";
-import rehypePrettyCode from "rehype-pretty-code";
+import toc from '@jsdevtools/rehype-toc';
+import rehypeStringify from 'rehype-stringify';
+import remarkRehype from 'remark-rehype';
+import remarkParse from 'remark-parse';
+import { unified } from 'unified';
+import rehypePrettyCode from 'rehype-pretty-code';
+import rehypeHighlight from 'rehype-highlight';
+import rehypeSlug from 'rehype-slug';
+import rehypeExternalLinks from 'rehype-external-links';
+import rehypeAutolinkHeadings from 'rehype-autolink-headings';
+import remarkBreaks from 'remark-breaks'
 
 export function markdownToHtml(markdown: string): Promise<string> {
   return unified()
-  .use(remarkParse)
-  .use(remarkRehype)
-  .use(rehypePrettyCode, {
-    theme: {
-      dark: "github-dark-dimmed",
-      light: "github-light",
-    }
-  })
-  .use(rehypeStringify)
+    .use(remarkParse)
+    .use(remarkBreaks)
+    .use(remarkRehype)
+    .use(rehypeSlug)
+    .use(rehypeAutolinkHeadings)
+    .use(rehypeExternalLinks, {
+      target: '_blank',
+      rel: ['noopener', 'noreferrer'],
+    })
+    .use(rehypePrettyCode, {
+      theme: {
+        dark: "github-dark",
+        light: "github-light",
+      },
+    })
+    .use(rehypeStringify)
 
     .process(markdown)
     .then((res) => res.toString());
